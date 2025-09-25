6e20085360c70ee446e97ee3e6bca7fd2a2aef3a
Narendra Satya Adikurniawan <162872291+narendrasatyaa@users.noreply.github.com>
2025-09-25 13:47:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image, ImageOps
-import fitz  # PyMuPDF
+import fitz  # dari PyMuPDF
 import io
 
 st.set_page_config(page_title="Image/PDF to WebP Converter", page_icon="🖼️")
